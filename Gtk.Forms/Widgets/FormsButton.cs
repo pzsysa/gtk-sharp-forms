aaf222a2ad06ade3cd32c22fc0c51ddf4e9ebb54
marecki.krzysztof <marecki.krzysztof@5d2955c8-1f8d-e654-fcdd-8fb12893e775>
2010-07-16 13:57:50 +0000

    Add FormsButton class to fix Button.Label set * Gtk.Forms/objects.xml: * Gtk.Forms/GtkForms.csproj: * Gtk.Forms/Pixmaps/button.png: * Gtk.Forms/Widgets/FormsWindow.cs: * Gtk.Forms/Widgets/FormsButton.cs:

---
+++ Gtk.Forms/Widgets/FormsButton.cs
@@ -0,0 +1,80 @@
+//  
+//  FormsButton.cs
+//  
+//  Author:
+//       Krzysztof Marecki <freefirma@gmail.com>
+// 
+//  Copyright (c) 2010 KrzysztofMarecki
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Linq;
+using Gtk;
+
+namespace GtkForms
+{
+	public class FormsButton : Button
+	{
+		private WidgetDecorator decorator;
+		internal WidgetDecorator Decorator { 
+			get {
+				if (decorator == null)
+					decorator = new WidgetDecorator (this);
+				
+				return decorator;
+			}
+		}
+		
+		public FormsButton ()
+			: base ()
+		{
+		}
+		
+		public FormsButton (IntPtr raw)
+			: base (raw)
+		{
+		}
+		
+		public new string Label {
+			get { return base.Label; }
+			set {
+				//otheriwse after setting the Label property, Image was cleared
+				Label label = FindLabelChild (this);
+				if (label != null) {
+					label.Text = value;
+				} else {
+					base.Label = value;
+				}
+			}
+		}
+	
+			Label FindLabelChild (Container container)
+			{
+				var widgets = (
+					from child in container.Children
+					where typeof (Label).IsAssignableFrom (child.GetType ())
+					select (Label) child
+				).Union (
+					from child in container.Children
+					where typeof (Container).IsAssignableFrom (child.GetType ())
+				    select FindLabelChild ((Container) child)
+				);
+				
+				var labels = widgets.ToArray ();
+				return labels.Length > 0 ? labels.First (w => (w != null)) : null;
+			}
+		
+	}
+}
+
